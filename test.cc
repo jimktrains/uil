25401854d7a75d938c5b68455559cc412d44d2b4
Jim Keener <jim@jimkeener.com>
2016-09-19 22:40:32 -0400

    Better logic for a 3-piece segment of track

--- test.cc
+++ test.cc
@@ -3,23 +3,109 @@
 #include <time.h>
 #include "LadderRung.h"
 
+char pairToSignal(Pentastate clear, Pentastate approach)
+{
+  char S0w = ' ';
+  if (isHigh(approach))
+  {
+    S0w = 'A';
+  }
+  else if (isHigh(clear))
+  {
+    S0w = 'C';
+  }
+  else
+  {
+    S0w = 'S';
+  }
+  return S0w;
+}
 
 int main()
 {
   srand(time(NULL));
 
-  auto button1 = RandomInput();
-  auto led1 = TestOutput();
+  auto T0w = FixedInput(Pentastate::Low);//SemiRandomInput();
+  auto T0e = FixedInput(Pentastate::Low);//_INV(T0w.value()));
+  auto T0 = TestOutput();
+
+  auto Tm1e = SemiRandomInput();
+  auto Tm1w = FixedInput(_INV(Tm1e.value()));
+  auto Tm1 = TestOutput();
+
+  auto T1e = SemiRandomInput();//FixedInput(Pentastate::Low);//SemiRandomInput();
+  auto T1w = FixedInput(_INV(T1e.value()));//FixedInput(Pentastate::Low);//_INV(T1e.value()));
+  auto T1 = TestOutput();
+
+  auto S0wc = TestOutput();
+  auto S0wa = TestOutput();
+
+  auto S0ec = TestOutput();
+  auto S0ea = TestOutput();
+
+  /*
+   *                    S0e
+   * |   T-1   |     T0    |   T1      |
+   *            S0w
+   */
 
   LadderRung<0>()
-    .LD(button1)
-    .AND(button1)
-    .OUT(led1)
-    .MPS()
-      .LD(button1)
-      .OR(button1)
-      .OUT(led1)
-    .ORB()
-    .OUT(led1);
- ;
+    // Sets occupancy if there is a train going in either direction
+    .LD(T0e)
+    .OR(T0w)
+    .OUT(T0)
+
+    // Sets occupancy if there is a train going in either direction
+    .LD(T1e)
+    .OR(T1w)
+    .OUT(T1)
+
+    // Sets occupancy if there is a train going in either direction
+    .LD(Tm1e)
+    .OR(Tm1w)
+    .OUT(Tm1)
+
+    // Clear iff no train for the next two blocks AND appoaching, not
+    // leaving the signal
+    .LDI(T0)
+    .ANDI(T1)
+    .ANDI(Tm1w)
+    .OUT(S0wc)
+
+    // Clear iff no train for the next one block AND appoaching, not
+    // leaving the signal AND train in two blocks is headed in the same
+    // direction
+    .LDI(T0)
+    .AND(T1e)
+    .ANDI(Tm1w)
+    .OUT(S0wa)
+
+    // Clear iff no train for the next two blocks AND appoaching, not
+    // leaving the signal
+    .LDI(T0)
+    .ANDI(Tm1)
+    .ANDI(T1e)
+    .OUT(S0ec)
+
+    // If I don't have a train
+    .LDI(T0)
+    // and T-1 has a westbound
+    .AND(Tm1w)
+    // and T1 doesn't have an east bound
+    .ANDI(T1e)
+    // east signal can be apprach
+    .OUT(S0ea)
+  ;
+
+
+  char S0e = pairToSignal(S0ec.value(), S0ea.value());
+  char S0w = pairToSignal(S0wc.value(), S0wa.value());
+
+  std::cout << "             " << S0e << std::endl;
+  std::cout << "|  " << (isHigh(Tm1e.value()) ? "->" : isHigh(Tm1w.value()) ? "<-" : "  ")
+          << "  |  " << (isHigh(T0e.value()) ? "->" : isHigh(T0w.value()) ? "<-" : "  ")
+          << "  |  " << (isHigh(T1e.value()) ? "->" : isHigh(T1w.value()) ? "<-" : "  ")
+          << "  |" << std::endl;
+  std::cout << "        " << S0w << std::endl;
 }
+
